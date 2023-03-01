c1a430b86f17f3e432b14ed1d8e9e1afb78a561f
Ali HH <hajhassan.ali@outlook.com>
2023-03-01 13:09:55 +0100

    done

--- algo1.md
+++ algo1.md
@@ -1,1 +1,16 @@
- 
+# Exercise 1
+#
+
+int x;
+int y;
+int m;
+
+    Fonction {
+
+        DEBUT
+        m = (x+y)/2;
+        Retourne m;
+
+        FIN
+    }
+
---
+++ algo1.php
@@ -0,0 +1,11 @@
+<?php
+   
+    function moyenne($x , $y ){
+
+        $result= intval(($x + $y)/2);
+        echo $result;
+
+    };
+
+    moyenne(10,5);
+    ?>

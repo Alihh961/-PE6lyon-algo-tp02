c65da90b57e195b3e9f58963e822d5b58e531025
Ali HH <hajhassan.ali@outlook.com>
2023-03-01 16:01:46 +0100

    adding test function to algo2

--- algo1.php
+++ algo1.php
@@ -2,10 +2,11 @@
    
     function moyenne($x , $y ){
 
-        $result= intval(($x + $y)/2);
+        $result= ($x + $y)/2;
         echo $result;
 
     };
 
     moyenne(10,5);
-    ?>
+
+?>
--- algo2.php
+++ algo2.php
@@ -9,59 +9,189 @@
 
     }
 
+    function testEstValide_TRUE() {
+        if (estvalide(10)) { 
+        echo "Test testEstValide_TRUE : OK"; 
+        }
+        else { echo"Test testEstValide_TRUE : Échec"; } 
+       }
+
+     testEstValide_TRUE();
+     echo PHP_EOL;
+
+     function testEstValide_FALSE_NEGATIVE() {
+        if (!estvalide(-10)) { 
+        echo "Test testEstValide_FALSE_NEGATIVE : OK"; 
+        }
+        else { echo"Test testEstValide_FALSE_NEGATIVE : Échec"; } 
+       }
+
+       testEstValide_FALSE_NEGATIVE();
+       echo PHP_EOL;
+
     function excellent($x){
 
-        if($x >= 18 ){
+        if($x >= 18 && $x <=20 ){
 
             return true;
         }
         return false;
 
     }
+
+    function testExcellent_TRUE(){
+        
+        if(excellent(19)){
+
+        echo "Test testExcellent_TRUE : OK"; 
+        }
+        else { echo "Test testExcellent_TRUE : Échec " ;}
+
+        }
+        testExcellent_TRUE();
+       echo PHP_EOL;
+
+
+    function testExcellent_FALSE_NEGATIVE(){ 
+        
+            if(!excellent(15)){
+    
+            echo "Test testExcellent_FALSE_NEGATIVE : OK"; 
+            }
+            else { echo "Test testExcellent_FALSE_NEGATIVE : Échec " ;}
+    
+            }
+        testExcellent_FALSE_NEGATIVE();
+       echo PHP_EOL;
+        
+
+    
 
     function tb($x){
 
-        if($x >= 16 ){
+        if($x >= 16 && $x <=18 ){
 
             return true;
         }
         return false;
 
     }
+
+    function testTB_TRUE(){
+
+        if(tb(17)){
+
+            echo "Test testTB_TRUE : OK"; 
+            }
+            else { echo "Test testTB_TRUE : Échec " ;}
+
+        }
+        testTB_TRUE();
+        echo PHP_EOL;
+
+        function testTB_FALSE_NEGATIVE(){
+
+            if(!tb(10)){
+    
+                echo "Test testTB_FALSE_NEGATIVE : OK"; 
+                }
+                else { echo "Test testTB_FALSE_NEGATIVE : Échec " ;}
+    
+            }
+
+        testTB_FALSE_NEGATIVE();
+        echo PHP_EOL;
+    
+
+
+    
 
     function bien($x){
 
-        if($x >= 14 ){
+        if($x >= 14 && $x <=16 ){
 
             return true;
         }
         return false;
     }
+
+    function testBien_TRUE(){
+
+        if(bien(15)){
+
+            echo "Test testBien_TRUE : OK"; 
+            }
+            else { echo "Test testBien_TRUE : Échec " ;}
+
+        }
+        testBien_TRUE();
+        echo PHP_EOL;
+
+        function testBien_FALSE_NEGATIVE(){
+
+            if(!bien(5)){
+    
+                echo "Test testBien_FALSE_NEGATIVE : OK"; 
+                }
+                else { echo "Test testBien_FALSE_NEGATIVE : Échec " ;}
+    
+            }
+            testBien_TRUE();
+            echo PHP_EOL;
+
 
     function ab($x){
 
-        if($x >= 12 ){
+        if($x >= 12 && $x <= 14 ){
 
             return true;
         }
         return false;
     }
+
+    function testAb_TRUE(){
+
+        if(ab(13)){
+
+            echo "Test testAb_TRUE : OK"; 
+            }
+            else { echo "Test testAb_TRUE : Échec " ;}
+
+        }
+        testAb_TRUE();
+        echo PHP_EOL;
+
+        function testAb_FALSE_NEGATIVE(){
+
+            if(!ab(2)){
+    
+                echo "Test testAb_FALSE_NEGATIVE : OK"; 
+                }
+                else { echo "Test testAb_FALSE_NEGATIVE : Échec " ;}
+    
+            }
+            testAb_FALSE_NEGATIVE();
+            echo PHP_EOL;
+
+
+
 
 
     function mention($x){
 
       if(!estvalide($x)){
         echo "Erreur";
-      }elseif(excellent($x)){
+      }else if(excellent($x)){
         echo "Execellent";
-      }elseif(tb($x)){
+      }else if(tb($x)){
         echo "TB";
-      }elseif(bien($x)){
+      }else if(bien($x)){
         echo 'Bien';
-      }elseif(ab($x)){
+      }else if(ab($x)){
         echo "AB";
       };
 
     }
 
-    mention(5);
+
+    mention(15);

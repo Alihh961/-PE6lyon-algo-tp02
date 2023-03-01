d75f3bd1bbe081bc8caec35aed8b3514cbdb5bda
Ali HH <hajhassan.ali@outlook.com>
2023-03-01 16:17:20 +0100

    algo1.php test function

--- algo1.php
+++ algo1.php
@@ -3,10 +3,34 @@
     function moyenne($x , $y ){
 
         $result= ($x + $y)/2;
-        echo $result;
+        return $result;
 
     };
 
-    moyenne(10,5);
+    // echo moyenne(10,5);
+
+    function testMoyenne_TRUE(){
+        if(moyenne(10 , 8) == 9){
+            echo "Test testMoyenne_TRUE : OK";
+        }else {
+            echo "Test testMoyenne_TRUE : Echec";
+        }
+    }
+
+    testMoyenne_TRUE();
+    echo PHP_EOL;
+
+    function testMoyenne_FALSE_NEGATIVE(){
+        if(!(moyenne(10 , 8) == 5)){
+            echo "Test testMoyenne_FALSE_NEGATIVE : OK";
+        }else {
+            echo "Test testMoyenne_FALSE_NEGATIVE : Echec";
+        }
+    }
+
+    testMoyenne_FALSE_NEGATIVE();
+    echo PHP_EOL;
+
+
 
 ?>

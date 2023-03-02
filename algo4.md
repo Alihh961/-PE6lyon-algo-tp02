1af4728e52cb430cf1b5522f2b2df13283a2887c
Ali HH <hajhassan.ali@outlook.com>
2023-03-02 10:58:34 +0100

    algo4.php solved

---
+++ algo4.md
@@ -0,0 +1,37 @@
+## Enoncé
+## Execrcise Algo4
+
+Fonction nEstValide(x)
+    ...
+    DEBUT
+    SI x % 1 == 0 OU x > 1
+    Retourne Vrai
+    FINSI
+    Retourne Faux
+    FIN
+    ...
+
+Fonction numInt(x)
+    ...
+    DEBUT
+    POUR ( y = 1 ; y <= x ; y++ )
+        Result = Result + y 
+    FINPOUR
+    Retourne Result
+    FIN
+    ...
+
+
+
+
+
+
+FONCTION sumNIntegers(n) :entier
+    ...
+    DEBUT
+    SI(nEstValide(x))
+        numInt(x)
+    FINSI
+    Retourne Faux
+
+        
---
+++ algo4.php
@@ -0,0 +1,72 @@
+<?php
+
+function nEstValide($x){
+    if(is_int($x) && ($x >= 1)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+function nEstValide_TRUE(){
+    if(nEstValide(5)){
+        echo "Test nEstValide_TRUE : OK";
+    }else{
+        echo "Test nEstValide_TRUE : Echec";
+    }
+}
+nEstValide_TRUE();
+echo PHP_EOL ;
+
+function nEstValide_NEGATIVE_FALSE(){
+    if(!nEstValide(-5)){
+        echo "Test nEstValide_NEGATIVE_FALSE : OK";
+    }else{
+        echo "Test nEstValide_NEGATIVE_FALSE : Echec";
+    }
+}
+nEstValide_NEGATIVE_FALSE();
+echo PHP_EOL ;
+
+function numInt($x){
+    $result = 0;
+    for($y = 1 ; $y <= $x ; $y++){
+    
+        $result +=   $y ;
+     
+    }
+    return $result ;
+}
+
+function numInt_TRUE(){
+    if(numInt(1) == 1){
+        echo "Test numInt_TRUE : OK";
+    }else{
+        echo "Test numInt_TRUE : Echec";
+    }
+}
+numInt_TRUE();
+echo PHP_EOL ;
+
+function numInt_NEGATIVE_FALSE(){
+    if(!numInt(1) == 0){
+        echo "Test numInt_NEGATIVE_FALSE : OK";
+    }else{
+        echo "Test numInt_NEGATIVE_FALSE : Echec";
+    }
+}
+numInt_NEGATIVE_FALSE();
+echo PHP_EOL ;
+
+function sumNIntegers($x){
+
+    if(nEstValide($x)){
+       echo numInt($x);
+    }else{
+        echo "Faux";
+    }
+};
+
+sumNIntegers(2.5);
+
